c9c7c99db8e978840b1b5cbd9881fdb9d4d00bbb
Shenghao <shenghaodong18@gmail.com>
2023-05-01 09:22:50 -0400

    fixed errors

--- stack.cpp
+++ stack.cpp
@@ -3,12 +3,13 @@
 #include "node.h"
 
 //Change all these to work with nodes
-void stack::push(int item){
-    if (topIndex >= 5){
+void stack::push(std::string item){
+    if (topIndex >= 5){//Change this number don't hard code it in
         throw STACK_ERR_FULL;
     }
 
-    head.setNext() -> item;
+    Node *nextNode = new Node(item);
+    head -> setNext(nextNode);
     //a[topIndex] = item;
     topIndex++;
 }
@@ -18,8 +19,9 @@ int stack::pop(){
         throw STACK_ERR_EMPTY;
     }
 
-    int tmp = a[topIndex-1];
-    topIndex--;
+    //int tmp = a[topIndex-1];
+    //topIndex--;
+    int tmp = 0;
     return tmp;
 }
 
@@ -27,8 +29,8 @@ int stack::top(){
     if (topIndex <= 0){
     throw STACK_ERR_EMPTY;
     }
-
-    return head.getNext();
+    int returnVal = std::stoi(head -> getData());
+    return returnVal;
     //return a[topIndex-1];
 }
 
--- stack.h
+++ stack.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <iostream>
 #include "node.h"
+
+
+#define STACK_ERR_EMPTY 1
+#define STACK_ERR_FULL 2
+#define STACK_ERR_OTHER 4
+
 class stack{
     private:
     Node *head;
@@ -8,7 +14,7 @@ class stack{
   
 
     public:
-    void push(int); // add an item to the top
+    void push(std::string); // add an item to the top
     int pop(); // remove and return an item from the top of the stack
     int top(); // returns but doesn't remove the value on top
     bool is_empty(); // returns true if stack is empty
