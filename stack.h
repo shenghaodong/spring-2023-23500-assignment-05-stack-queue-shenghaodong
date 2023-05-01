aa54e4dd36a88e4f0b02cb732846a784914faa72
Shenghao Dong <shenghaodong18@gmail.com>
2023-04-30 23:00:06 -0400

    added some stuff in stack

--- stack.h
+++ stack.h
@@ -1,4 +1,14 @@
 #pragma once
 class stack{
+    private:
+    Node *head;
+    int topIndex;
+  
+
+    public:
+    void push(int); // add an item to the top
+    int pop(); // remove and return an item from the top of the stack
+    int top(); // returns but doesn't remove the value on top
+    bool is_empty(); // returns true if stack is empty
     
 };
