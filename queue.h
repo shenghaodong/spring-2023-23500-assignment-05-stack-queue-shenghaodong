75832f8f0f888c9e16677d70251feb1219a40006
Shenghao Dong <shenghaodong18@gmail.com>
2023-05-01 23:07:10 -0400

    accidently edited readme

--- queue.h
+++ queue.h
@@ -12,8 +12,9 @@ Your class must implement the following functionality:
 
 class queue{
     private:
-    //array
-    int topIndex = 0; //topIndex of 0 means that there is nothing in the stack
+    int arr[10];
+    int head;
+    int tail;
   
     
     public:
@@ -22,5 +23,11 @@ class queue{
     int front();
     bool is_empty();
     bool is_full();
-    
 };
+
+/*
+Create an array (or std::vector) based queue class. You can call the class whatever you like. 
+It should start the array (or std::vector) at some small size and have integer index variables to represent the front and back (head / tail). 
+When you get to the end of the array, you should cycle back to the beginning and when the head wraps around and catches up with the tail, the queue should be considered full. 
+Likewise if the tail catches up with the head the qwueue is empty. Note that the tail also wraps around and taht the queue starts out empty.
+*/
